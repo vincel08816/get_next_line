32e5a646e884fc77fb6a088d6a01b372c44c66f7
Vincent Lee <vilee@e1z3r8p14.42.us.org>
2019-09-14 13:56:52 -0700

    to check valgrind

--- get_next_line.c
+++ get_next_line.c
@@ -31,12 +31,19 @@ static size_t	strichr(char *s, char c)
 
 static void		ft_combine(char **tmp, char *buff)
 {
-	char	*del;
+	char *del;
 
 	del = *tmp;
 	*tmp = ft_strjoin(*tmp, buff);
-	//free(del);
+	free(del);
 }
+
+/*
+**	reject_line strdup's up to /n by using
+**	strichr to find the index of \n
+**	using the pointer after the \n and copying the rest to
+**	the static variable s[fd] from the main GNL
+*/
 
 static int		reject_line(int size, char **line, char **tmp, char **s)
 {
@@ -44,64 +51,70 @@ static int		reject_line(int size, char **line, char **tmp, char **s)
 	char	*start;
 
 	if (!size)
+	{
+		free(*tmp);
 		return (0);
+	}
 	i = 0;
 	start = *tmp;
 	i = strichr(*tmp, '\n');
 	*line = ft_strsub(*tmp, 0, i);
 	*tmp += i + 1;
+	*s ? free(*s) : 0;
 	if (**tmp != '\0' || **tmp != '\n')
 		*s = ft_strdup(*tmp);
 	ft_strdel(&start);
 	return (1);
-}ß
+}
+
+/*
+**	function uses read() to return 0, 1, -1 for EOF, \n, err
+**	read() loop breaks when \n or \0 is found
+**	strichr i will find \n and size == 0 will signify the end of file
+**	the static character will read
+*/
 
 int				get_next_line(int fd, char **line)
 {
 	static char *s[MAX_FD];
 	char		buff[BUFF_SIZE + 1];
 	char		*tmp;
-	char		*tmp2;
 	int			size;
 
-	tmp2 = NULL;
 	size = 1;
 	if (fd < 0 || fd > MAX_FD || !line || (read(fd, 0, 0) < 0))
 		return (-1);
 	tmp = ft_strdup("");
 	s[fd] ? ft_combine(&tmp, s[fd]) : 0;
-	s[fd] ? free(s[fd]) : 0;
 	if (strichr(tmp, '\n') < ft_strlen(tmp))
 		return (reject_line(size, line, &tmp, &s[fd]));
 	while ((size = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[size] = '\0';
-		//tmp2 = tmp;
 		ft_combine(&tmp, buff);
-		//tmp = ft_strjoin(tmp, buff);
-		//free(tmp2);
 		if (strichr(buff, '\n') < ft_strlen(buff))
 			break ;
 	}
 	if (!s[fd])
 		return (reject_line(1, line, &tmp, &s[fd]));
 	(ft_strcmp(tmp, "")) ? size++ : 0;
-	size == 0 ? ft_strdel(&tmp) : 0;
 	return (reject_line(size, line, &tmp, &s[fd]));
 }
-/*
-#include <stdio.h>
-int main(int argc, char **argv)
-{
-    char *line;
-    int fd;
 
-    fd = open(argv[1], O_RDONLY);
-    while (get_next_line(fd, &line) > 0)
-    {
-        printf("%s\n", line);
-        free(line);
-    }
-    return (0);
-}
+/*
+**	#include <stdio.h>
+**
+**	int main(int argc, char **argv)
+**	{
+**	    char *line;
+**	    int fd;
+**
+**	    fd = open(argv[1], O_RDONLY);
+**	    while (get_next_line(fd, &line) > 0)
+**	    {
+**	        printf("%s\n", line);
+**	        free(line);
+**	    }
+**	    return (0);
+**	}
 */
--- get_next_line.h
+++ get_next_line.h
@@ -10,26 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef   GET_NEXT_LINE_H
-# define  GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include "fcntl.h"
 # include "sys/types.h"
 # include "sys/uio.h"
 # include "unistd.h"
-# include <stdlib.h>
-# include <stdio.h>
+# include "stdlib.h"
 # include "libft/libft.h"
 
-# define BUFF_SIZE 32
-# define GNL_MACRO char buff[BUFF_SIZE]; char *ret; size_t size; size_t schr;
+# define BUFF_SIZE 1
 # define MAX_FD 65535
-
-typedef struct		s_node
-{
-	char			*s;
-	struct s_node	*next;
-}					node;
 
 int				get_next_line(const int fd, char **line);
 
